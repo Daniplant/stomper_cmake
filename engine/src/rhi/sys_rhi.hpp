dcb0dd82f9883a3af2a3c232092eb1b45901774c
Delta <deltawaveanimations@gmail.com>
2025-10-01 11:55:50 +0200

    Added common TextureBase struct

--- engine/src/rhi/sys_rhi.hpp
+++ engine/src/rhi/sys_rhi.hpp
@@ -1,19 +1,28 @@
 #pragma once
 #include "engine/common.hpp"
+#include "engine/rhi/rhi.hpp"
+
 #include <format>
 #include <spdlog/spdlog.h>
 
-inline auto rhi_logger = spdlog::default_logger()->clone("RHI");
+namespace core::rhi {
+    inline auto rhi_logger = spdlog::default_logger()->clone("RHI");
 
-constexpr u8 MAX_SWAPCHAIN_FRAMES = 3;
-constexpr u32 MAX_SAMPLER_DESCRIPTORS = 256;
-constexpr u32 MAX_SAMPLED_IMAGE_DESCRIPTORS  = 125000;
-constexpr u32 MAX_STORAGE_IMAGE_DESCRIPTORS  = 125000;
-constexpr u32 MAX_STORAGE_BUFFER_DESCRIPTORS = 125000;
-constexpr u32 MAX_UNIFORM_BUFFER_DESCRIPTORS = 125000;
+    constexpr u8 MAX_SWAPCHAIN_FRAMES = 3;
+    constexpr u32 MAX_SAMPLER_DESCRIPTORS = 256;
+    constexpr u32 MAX_SAMPLED_IMAGE_DESCRIPTORS  = 125000;
+    constexpr u32 MAX_STORAGE_IMAGE_DESCRIPTORS  = 125000;
+    constexpr u32 MAX_STORAGE_BUFFER_DESCRIPTORS = 125000;
+    constexpr u32 MAX_UNIFORM_BUFFER_DESCRIPTORS = 125000;
 
-#define RHI_INFO(...) rhi_logger->info(__VA_ARGS__);
-#define RHI_WARN(...) rhi_logger->warn(__VA_ARGS__);
-#define RHI_ERROR(...) rhi_logger->error(__VA_ARGS__);
-#define RHI_DEBUG(...) rhi_logger->debug(__VA_ARGS__);
-#define RHI_CRITICAL(...) rhi_logger->critical(__VA_ARGS__);
+    #define RHI_INFO(...) rhi_logger->info(__VA_ARGS__);
+    #define RHI_WARN(...) rhi_logger->warn(__VA_ARGS__);
+    #define RHI_ERROR(...) rhi_logger->error(__VA_ARGS__);
+    #define RHI_DEBUG(...) rhi_logger->debug(__VA_ARGS__);
+    #define RHI_CRITICAL(...) rhi_logger->critical(__VA_ARGS__);
+
+
+    struct TextureBase {
+        TextureDescriptor desc;
+    };
+}
